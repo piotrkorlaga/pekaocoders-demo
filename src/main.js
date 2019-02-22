da6a3248ef7ce05f6c974b8c6bc0f9452ab73b08
Daniel Karski <daniel.karski5q@gmail.com>
2019-02-22 08:08:20 +0100

    :sparkles: Handled submit event - display success state!

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 import './main.scss';
+
+const formViewElement = window.document.getElementById('form-view');
+const sendButtonElement = window.document.getElementById('sendButton');
+
+sendButtonElement.addEventListener('click', function(){
+    sendButtonElement.disabled = true;
+    formViewElement.classList = 'form-view submitted';
+});
